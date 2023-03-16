3b63c07e8394139321c09ac9d72ff4e45f9b18c3
Karolina Hyla <karatakarabe@gmail.com>
2023-03-16 21:11:01 +0100

    Find the correct article using selector and add class active to the correct article

--- js/script.js
+++ js/script.js
@@ -19,8 +19,11 @@ const titleClickHandler = function(event) {
     // [DONE] get href atribute from the clicked link 
     const articleSelector = clickedElement.getAttribute('href')
     console.log(articleSelector);
-    // [IN PROGRESS] find the correct article using selector (href)
-    // [IN PROGRESS] add class active to the correct article
+    // [DONE] find the correct article using selector (href)
+    const targetArticle = document.querySelector(articleSelector)
+    console.log(targetArticle);
+    // [DONE] add class active to the correct article
+    targetArticle.classList.add('active');
 
 }
 
