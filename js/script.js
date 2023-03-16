15292201e03b17c64fb793b095f95b3f97a5563c
Karolina Hyla <karatakarabe@gmail.com>
2023-03-16 20:25:30 +0100

    Add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 'use strict';
-const titleClickHandler = function() {
-    console.log('Link was clicked!', window.event)
+const titleClickHandler = function(event) {
+    console.log('Link was clicked!', event)
 
     // [DONE] remove class 'active' form all links
     const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    // [IN PROGRESS] add class active to the clicked link
-    // [DONE] remove cladd active form all articles 
+    // [DONE] add class active to the clicked link
+    const clickedElement = this;
+    clickedElement.classList.add('active');
+    // [DONE] remove class active form all articles 
     const activeArticles = document.querySelectorAll('.post.active');
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
     // [IN PROGRESS] get href atribute from the clicked link 
+
     // [IN PROGRESS] find the correct article using selector (href)
     // [IN PROGRESS] add class active to the correct article
 }
